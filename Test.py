7c6e0ad7feacda1cd41b16c3213ef7c20eaac9c8
lkoh <lkoh@sierrawireless.com>
2022-12-02 14:19:43 +0800

    change

--- Test.py
+++ Test.py
@@ -1,13 +1,29 @@
 import paramiko
+import argparse
 
-def main():
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--generate_report',
+                    choices=['latest', 'last', 'none'],
+                    help='Generate an HTML test report')
+    parser.add_argument('Testbed_ID', 
+                        help='List the Tesbed_ID to run the tests')
+    parser.add_argument('Tests', nargs='*',
+                        help='List of test/campaigns to run')
+    return parser.parse_args()
+
+def get_test_report(ip_addr):
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect('10.29.51.83', username='vagrant', password='vagrant')
+    ssh_client.connect(ip_addr, username='vagrant', password='vagrant')
 
-    stdin, stdout, stderr = ssh_client.exec_command('ls')
+    ftp_client=ssh_client.open_sftp()
 
-    print(stdout.readlines())
+def main():
+    args = get_args()
+
+    testbed_ip = '10.29.51.83'
+    get_test_report(testbed_ip)
 
 if __name__ == '__main__':
     main()
