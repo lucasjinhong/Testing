b95c90590acfd8a529f4946f52408ad573b0bd04
lkoh <lkoh@sierrawireless.com>
2022-12-02 14:29:39 +0800

    123

--- Test.py
+++ Test.py
@@ -1,6 +1,11 @@
-import paramiko
-import argparse
+import subprocess
+import datetime
 import os
+import argparse
+import paramiko
+from re import findall
+
+SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -15,9 +20,8 @@ def get_args():
 
 def get_testbed_ip(id):
     try:
-        ip_addr = '10.29.51.83'
-        # ip_addr = subprocess.check_output(['./bed.sh', '-b', id])
-        # ip_addr = ip_addr.decode('ascii').split('\t')[1]
+        ip_addr = subprocess.check_output(['cd', '../bed-control', './bed.sh', '-b', id])
+        ip_addr = ip_addr.decode('ascii').split('\t')[1]
     except:
         raise ValueError('Testbed_ID not found')
 
@@ -73,12 +77,112 @@ def get_test_report(ip_addr, time, remote_path, version_name):
 
     return local_path
 
-def main():
-    args = get_args()
-    cmd_ssh = '1wqe'
-    testbed_ip = get_testbed_ip('qwr')
-    cmd_ssh = cmd_ssh + testbed_ip
-    get_test_report(testbed_ip, '1', '1', '1')
+def publish_test_report(version_name, local_path):
+    jasmine2_path = '//jasmine2/Projects-1/Engineering/Firmware/Releases/Internal/HL78XX/integration/Jenkins_Local_Test/'
+    time = local_path.split('/')[-3]
+    jasmine2_dir_name = local_path.split('/')[-2]
 
+    subprocess.run(['smbclient', jasmine2_path, '-c', 'prompt OFF'
+                    + '; mkdir ' + version_name + '; cd ' + version_name 
+                    + '; mkdir ' + time + '; cd ' + time
+                    + '; mkdir ' + jasmine2_dir_name + '; cd ' + jasmine2_dir_name 
+                    + '; lcd ' + local_path 
+                    + '; mput *', '-Ulkoh', '-W SIERRAWIRELESS', 'Jinhong253'
+                    ])
+    return
+
+def print_result(process):
+    flag = 0
+    log_path = ''
+    report_path = ''
+
+    for line in iter(process.stdout.readline, ''):
+        if line == 'END\n':
+            break
+        print(line, end='')
+
+        # Error Handler
+        if 'Error' in line:
+            flag = 1
+
+        # get path of logs and reports
+        if 'letp_wrapper_logs' in line:
+            log_path = line
+        elif 'letp_wrapper_reports' in line:
+            report_path = line
+
+    log_path = ''.join(x for x in findall(r'[\w+.+]+/', log_path)[-4:])
+    report_path = ''.join(x for x in findall(r'[\w+.+]+/', report_path)[-2:])
+
+    return log_path, report_path, flag
+
+def main():
+    os.chdir(SCRIPT_PATH)
+    args = get_args()
+    time = str(datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+    local_path = []
+
+    if len(args.Tests) < 1:
+        raise ValueError('Test not found')
+
+    cmd_letp = 'letp_wrap -y'
+    cmd_ssh =  'sshpass -p vagrant ssh -tt vagrant@'
+
+    if args.generate_report == 'latest':
+        cmd_letp += ' -r latest'
+    elif args.generate_report == 'last':
+        cmd_letp += ' -r last'
+
+    for i in args.Tests:
+        cmd_letp = cmd_letp + ' ' + i
+
+    testbed_ip = get_testbed_ip(args.Testbed_ID)
+    cmd_ssh = cmd_ssh + testbed_ip
+
+    print('\nTest to run: ', args.Tests)
+    print('Connect to Testbed: ', args.Testbed_ID, '\n')
+
+    ssh_process = subprocess.Popen(cmd_ssh,
+                                shell=True,
+                                stdin=subprocess.PIPE, 
+                                stdout=subprocess.PIPE,
+                                universal_newlines=True,
+                                bufsize=0)
+
+    print('Test START\n')
+    # Call letp_wrap command
+    ssh_process.stdin.write('cd WorkDir/integration/letp_wrapper\n')
+    ssh_process.stdin.write('source "$(pipenv --venv)/bin/activate"\n')
+    ssh_process.stdin.write('source init_letp.sh\n')
+    ssh_process.stdin.write(cmd_letp + '\n')
+
+    # Clear Pycache File
+    ssh_process.stdin.write('rm -rf ~/WorkDir/integration/letp/sanity/HL78xx/__pycache__ \n')
+    ssh_process.stdin.write('ls ~/WorkDir/integration/letp/sanity/HL78xx/__pycache__ \n')
+
+    ssh_process.stdin.write('logout\n\n')
+    ssh_process.stdin.close()
+
+    # Take path of log and report and print_result of test
+    log_path, report_path, flag = print_result(ssh_process)
+    print('\nTest END')
+
+    version_name = log_path.split('/')[2]
+
+    # Getting remote directory to local
+    print('\nStart Getting Logs and Reports directory')
+    for path in [log_path, report_path]:
+        if len(path) > 0:
+            local_path.append(get_test_report(testbed_ip, time, path, version_name))
+
+    print('\nStart Publishing Logs and reports to Jasmine2')
+    for path in local_path:
+        print('\nPublishing: ' + path.split('/')[-2])
+        publish_test_report(version_name, path)
+
+    print('\nDONE\n')
+    if flag == 1:
+        raise Exception('Test FAILED')
+    
 if __name__ == '__main__':
     main()
